a06237ac1c978f106d4a5b8a4f4372941311cf77
martynasjascemskas <martis.jascemskas@gmail.com>
2023-11-19 00:33:38 +0000

    models and routes for api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,45 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
+const mongoose = require('mongoose')
+const postcodesRoutes = require('./routes/postcodes')
+const houseRoutes = require('./routes/houses')
+
+//express app
 const app = express()
 
+//middleware
+app.use(express.json())
 
-
-app.listen(process.env.PORT, () => {
-    console.log('listen on port 4000')
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+//routes
+app.use('/api/postcodes', postcodesRoutes)
+app.use('/api/houses', houseRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+/*
+HOUSES:
+GET HOUSES BY POSTCODE
+(MAYBE) GET HOUSE INFO BY ID
+(MAYBE) CREATE NEW HOUSE SALE
+
+POSTCODES:
+GET ALL POSTCODES
+(MAYBE) GET 1 POSTCODE INFO BY ID
+
+*/
