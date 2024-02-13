2863d9f16d25829515d52b776cc28ddd63ab9174
martynasjascemskas <113460995+martynasjascemskas@users.noreply.github.com>
2024-02-13 10:31:53 +0000

    Frontend edit (#14)
    * gitignore
    
    * javascript-typescript and edit
    
    * removing clustering and package changes
    
    * full postcode info backend/frontend
    
    * frontend
    
    * readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # FYP
+
 Final year project in collaboration with Phil Maguire.
 Visualization tool for Post code price average.
 
 [Trello board](https://trello.com/b/W8BPwKf3/final-year-project-martynas)
 
 # Google Drive CSV Files:
+
 [Google Drive](https://drive.google.com/drive/folders/1Eys_iTOJ0MV4imoWZ1SbSlDGU6ZDrpk-?usp=sharing)
+
+frontend: `npm install --legacy-peer-deps`
+backend: `npm install` && `npm run dev`
--- backend/controllers/postcodeController.js
+++ backend/controllers/postcodeController.js
@@ -1,34 +1,43 @@
-const postcodeModel = require('../models/postcodeModel')
-const mongoose = require('mongoose')
+const postcodeModel = require("../models/postcodeModel");
+const mongoose = require("mongoose");
 
 const getAllPostcodes = async (req, res) => {
-    const postcodes = await postcodeModel.find({}).limit(100000) 
-    const filtered = postcodes.map(postcode => ({
-        postcode: postcode.pcds,
-        lat: postcode.lat,
-        long: postcode.long
-    }))
-    res.status(200).json(filtered)
-    //res.status(200).json(postcodes)
-}
+  const postcodes = await postcodeModel.find({}).limit(1000);
+  const filtered = postcodes.map((postcode) => ({
+    postcode: postcode.pcds,
+    lat: postcode.lat,
+    long: postcode.long,
+    avg_price_all_years: postcode.avg_price_all_years,
+    avg_price_2015: postcode.avg_price_2015,
+    avg_price_2016: postcode.avg_price_2016,
+    avg_price_2017: postcode.avg_price_2017,
+    avg_price_2018: postcode.avg_price_2018,
+    avg_price_2019: postcode.avg_price_2019,
+    avg_price_2020: postcode.avg_price_2020,
+    avg_price_2021: postcode.avg_price_2021,
+    avg_price_2022: postcode.avg_price_2022,
+  }));
+  res.status(200).json(filtered);
+  //res.status(200).json(postcodes)
+};
 
 const getSinglePostcode = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such postcode id.'})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such postcode id." });
+  }
 
-    const postcodes = await postcodeModel.findById(id)
+  const postcodes = await postcodeModel.findById(id);
 
-    if(!postcodes){
-        return res.status(404).json({error: 'No such postcode'})
-    }
+  if (!postcodes) {
+    return res.status(404).json({ error: "No such postcode" });
+  }
 
-    res.status(200).json(postcodes)
-}
+  res.status(200).json(postcodes);
+};
 
 module.exports = {
-    getAllPostcodes,
-    getSinglePostcode
-}
+  getAllPostcodes,
+  getSinglePostcode,
+};
